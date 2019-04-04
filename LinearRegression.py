9e0066f95c6ba90dea90c3e8f7439b0dea30755c
Arun <queroego@gmail.com>
2019-04-04 19:19:28 +0800

    Update LinearRegression.py

--- LinearRegression.py
+++ LinearRegression.py
@@ -9,3 +9,9 @@ from math import sqrt
 
 #read the advertising data to dataframe
 adv_df = pd.read_csv('Advertising.csv',index_col=0)
+
+#visualize the relationship between features and sales using scatter plot
+fig,axes = plt.subplots(1,3,sharey=True)
+adv_df.plot(kind='scatter',x='TV',y='sales',ax=axes[0],figsize=(16,8))
+adv_df.plot(kind='scatter',x='radio',y='sales',ax=axes[1])
+adv_df.plot(kind='scatter',x='newspaper',y='sales',ax=axes[2])
