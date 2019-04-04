8373adaf0c4b5586097f6e116a0635a87e50fb11
Arun <queroego@gmail.com>
2019-04-04 19:23:44 +0800

    Update LinearRegression.py

--- LinearRegression.py
+++ LinearRegression.py
@@ -15,3 +15,17 @@ fig,axes = plt.subplots(1,3,sharey=True)
 adv_df.plot(kind='scatter',x='TV',y='sales',ax=axes[0],figsize=(16,8))
 adv_df.plot(kind='scatter',x='radio',y='sales',ax=axes[1])
 adv_df.plot(kind='scatter',x='newspaper',y='sales',ax=axes[2])
+
+#Working with the feature variable 'TV' against the sales to find any relationship between them
+#Create X('TV') and Y('Sales') variables
+X = adv_df[['TV']]
+Y = adv_df[['sales']]
+
+#Now import, instantiate and fit the data
+from sklearn.linear_model import LinearRegression
+lm = LinearRegression()
+lm.fit(X,Y)
+
+#print the intercept and coefficents
+print('Intercept:',lm.intercept_)
+print('Coefficient:',lm.coef_)
