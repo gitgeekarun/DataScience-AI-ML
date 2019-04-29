193e6f871a034eeb4a929ccd57aed0bab3056707
Arun <queroego@gmail.com>
2019-04-29 17:03:46 +0800

    Create linear_polynomial_regression.py
    Linear_Polynomial_Regression

---
+++ Regression/linear_polynomial_regression.py
@@ -0,0 +1,8 @@
+#import all required libraries
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+#models
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import PolynomialFeatures
